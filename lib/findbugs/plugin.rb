a2514dc500795f9dc69f3f489ab2606d7741f670
kazuki-yoshida <kzk.yshd@gmail.com>
2017-05-07 23:12:56 +0900

    Write plguin file

--- lib/findbugs/plugin.rb
+++ lib/findbugs/plugin.rb
@@ -1,33 +1,56 @@
 module Danger
-  # This is your plugin class. Any attributes or methods you expose here will
-  # be available from within your Dangerfile.
-  #
-  # To be published on the Danger plugins site, you will need to have
-  # the public interface documented. Danger uses [YARD](http://yardoc.org/)
-  # for generating documentation from your plugin source, and you can verify
-  # by running `danger plugins lint` or `bundle exec rake spec`.
-  #
-  # You should replace these comments with a public description of your library.
-  #
-  # @example Ensure people are well warned about merging on Mondays
-  #
-  #          my_plugin.warn_on_mondays
-  #
-  # @see  kazuki-yoshida/danger-findbugs
-  # @tags monday, weekends, time, rattata
-  #
   class DangerFindbugs < Plugin
+    attr_accessor :gradle_task
+    attr_accessor :report_file
 
-    # An attribute that you can read/write from your Dangerfile
-    #
-    # @return   [Array<String>]
-    attr_accessor :my_attribute
-
-    # A method that you can call from your Dangerfile
-    # @return   [Array<String>]
-    #
-    def warn_on_mondays
-      warn 'Trying to merge code on a Monday' if Date.today.wday == 1
+    def report_file
+      return @report_file || 'app/build/reports/findbugs/findbugs.xml'
     end
+
+    def report
+      unless gradlew_exists?
+        fail("Could not find `gradlew` inside current directory")
+        return
+      end
+
+      system "./gradlew #{gradle_task || 'findbugs'}"
+
+      unless File.exists?(report_file)
+        fail("Findbugs report not found at `#{report_file}`. "\
+        "Have you forgot to add `xmlReport true` to your `build.gradle` file?")
+      end
+
+      issues = read_issues_from_report
+      send_inline_comment(issues)
+    end
+
+    private
+
+    def read_issues_from_report
+      file = File.open(report_file)
+
+      require 'oga'
+      report = Oga.parse_xml(file)
+
+      report.xpath('//BugInstance')
+    end
+
+    def send_inline_comment (issues)
+      target_files = (git.modified_files - git.deleted_files) + git.added_files
+      dir = "#{Dir.pwd}/"
+      issues.each do |issue|
+        # todo
+        # next unless target_files.include? filename
+        # location = r.xpath('location').first
+        # filename = location.get('file').gsub(dir, "")
+        # line = (location.get('line') || "0").to_i
+        # send("fail", r.get('message'), file: filename, line: line)
+      end
+    end
+
+    def gradlew_exists?
+      `ls gradlew`.strip.empty? == false
+    end
+
   end
 end
